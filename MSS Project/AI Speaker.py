72850bef0f2ac661b43d3c01726f122109c8a971
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2022-11-19 19:04:31 +0900

    비동기 방식으로 변경 => mqtt서버로부터 항시 대기할수 있도록

--- MSS Project/AI Speaker.py
+++ MSS Project/AI Speaker.py
@@ -10,6 +10,7 @@ from playsound import playsound
 import paho.mqtt.client as mqtt
 
 isCall = False  # 호출어가 명령된 상태인지 확인
+wai = True  # mqtt 수신 명령을 말하는동안 스피커가 말하는 소리를 음성으로 인식하지 않도록
 power = True  # 스피커의 전원에 해당
 answer_text = '입력이 되지 않았습니다'
 r = sr.Recognizer()
@@ -17,9 +18,8 @@ r = sr.Recognizer()
 
 broker_address = "192.168.216.87"  # input("브로커 IP>>")
 
+
 # 스피커에서는 온습도정보, 손님방문정보를 구독함
-
-
 def onConnect(client, userdata, flag, rc):
     if (rc == 0):
         print(" 연결되었습니다")
@@ -29,36 +29,28 @@ def onConnect(client, userdata, flag, rc):
 
 
 def onMessage(client, userdata, msg):
-    global distanceLabel
-    command = str(msg.payload.decode("utf-8"))
+    global distanceLabel, wait
+    command = str(msg.payload.decode("utf-8"))  # 소수점 제거
     # 이미지 데이터가 바이트 배열로 도착, 이것은 보내는쪽에서 바이트배열로 보내야함을 의미함
     if (msg.topic == "temperature"):
-        print("현재 날씨 " + command)
+        #print("현재 날씨 " + command)
+        speak("현재 날씨는 " + command + "도 입니다.")
+        wait = True  # 다시 사용자로부터 음성을 들을수 있도록 함
     pass
 
 
-# 구독한 정보가 도착했을 때 작동할 함수
-# def on_message(client, userdata, msg):
-#     global distanceLabel, answer_text
-#     command = str(msg.payload.decode("utf-8"))
-#     # 온도 정보가 도착했을때,
-#     if (msg.topic == "temperature"):
-#         #answer_text = command  # 전달받은 온도값을 스피커의 대답으로
-#         print("현재 온도는 " + command)
-#         pass
-
-#     # # 거리정보
-#     # if (msg.topic == "distance"):
-#     #     distanceLabel.config(text=str(msg.payload.decode("utf-8")))
-#     #     pass
-#     pass
-
-
 def listen(recognizer, audio):  # 음성 인식 (듣기 )
+    global wait
     try:
         text = recognizer.recognize_google(audio, language='ko')
         print("[User] " + text)
-        answer(text)
+
+        # mqtt 수신 명령에 대한 처리
+        if '날씨' in text:
+            client.publish("command", "temperature", qos=0)
+            wait = False  # 날씨 정보에 관해 스피커가 말하는것을 음성명령으로 인식하지 않도록
+        else:  # mqtt 송수신 명령 이외의 명령의 경우 => 불 켜줘, 등~
+            answer(text)
     except sr.UnknownValueError:
         print("인식 실패")  # 음성 인식이 실패한 경우
         speak("잘 듣지 못했어요")
@@ -68,13 +60,11 @@ def listen(recognizer, audio):  # 음성 인식 (듣기 )
 
 
 def answer(input_text):  # 어떤 대답을 할것인지 정의
-    global isCall, power
+    global isCall, power, wait
     global answer_text  # 컴퓨터가 대답할 말 key값이 들어갔다면 출력되도록
+
     if '안녕' in input_text:
         answer_text = "안녕하세요? 반갑습니다."
-    elif '날씨' in input_text:
-        client.publish("command", "temperature", qos=0)
-        # answer_text = "오늘 서울 기온은 20도입니다. 맑은 하늘이 예상됩니다."
     elif '불' in input_text:
         if '켜' in input_text:
             answer_text = "불을 켭니다"
@@ -82,13 +72,6 @@ def answer(input_text):  # 어떤 대답을 할것인지 정의
         elif '꺼' in input_text:
             answer_text = "불을 끕니다"
             client.publish("command", "ledOff")
-    # elif '켜' in input_text:
-    #     answer_text = "불을 켭니다"
-    #     client.publish("command", "ledOn")
-    # elif '불' in input_text:
-    #     answer_text = "불을 끕니다"
-    #     client.publish("command", "ledOff")
-
     elif '환율' in input_text:
         answer_text = "원 달러 환율은 1400원입니다."
     elif '고마워' in input_text:
@@ -98,11 +81,10 @@ def answer(input_text):  # 어떤 대답을 할것인지 정의
         isCall = False
         power = False
         # stop_listening(wait_for_stop=False)  # 더이상 듣지 않음
-    elif '부기' in input_text:
+    elif '자비스' in input_text:
         answer_text = "부르셨나요?"
-    else:
-        answer_text = "잘 이해하지 못했어요."
-
+    # else:
+    #     answer_text = "잘 이해하지 못했어요."
     speak(answer_text)
 
 
@@ -128,11 +110,10 @@ client = mqtt.Client()      # mqtt 클라이언트 객체 생성
 client.on_connect = onConnect   # 연결요청시 Callback 함수
 client.on_message = onMessage   # 이미지가 도착하였을때 Callback 함수
 client.connect(broker_address, 1883)   # 브로커에 연결을 요청함
-
+client.loop_start()  # 인공지능 스피커가 동작해야하므로 비동기식으로 별도의 스레드로 진행
 
 # 스피커가 동작하는 공간
 while power:
-    client.loop()  # 메시지가 왔는지 체크함
     with sr.Microphone() as source:  # 마이크에서 들리는 음성(source)을 listen을 통해 들음
         print('Listening')  # 잠깐의 대기 시간이 있으므로 확인용으로 텍스트 출력
         callName = r.listen(source)  # 마이크로부터 호출어 듣기
@@ -152,19 +133,21 @@ while power:
 
         if "자비스" in text:  # 음성이 키워드일때 명령을 할수있게 변경
             isCall = True
+            wait = True
             speak("안녕하세요!")
 
             # 아무 명령을 하지 않더라도, 20초가 지나면 명령 권한 회수
-            start_time = threading.Timer(20, reset_mode)  # 20초후 reset
+            start_time = threading.Timer(50, reset_mode)  # 20초후 reset
             start_time.start()
         else:
             continue
 
     # 명령 권한이 있는 상태에서는 계속 명령을 받을 수 있음
     while isCall:
-        client.loop()  # 메시지가 왔는지 체크함
-        with sr.Microphone() as source:
-            command = r.listen(source)  # 마이크로부터 음성 듣기
-            listen(r, command)
+        while wait:
+            with sr.Microphone() as source:
+                command = r.listen(source)  # 마이크로부터 음성 듣기
+                listen(r, command)
 
 client.disconnect()
+client.loop_stop()
