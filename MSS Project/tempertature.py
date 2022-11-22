ebe7f5f9c9fad0dd86348d5b2fda0d208e24cb2f
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2022-11-22 22:01:33 +0900

    별도의 라즈베리파이에서  독립시행시 작동할수 있도록 수정

--- MSS Project/tempertature.py
+++ MSS Project/tempertature.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import busio
+import time
 from adafruit_htu21d import HTU21D
 
 sda = 2  # GPIO핀 번호
@@ -14,3 +15,40 @@ def getTemperatureAndHumidity():
     humidity = float(sensor.relative_humidity)  # HTU21D 장치로부터 습도 값 읽기
     return temperature  # 우선 온도 정보만
 # , humidity  # 온습도 정보 리턴
+
+
+# 별도 작동용 => controller로 연결하지 않고 다수의 라즈베리파이를 사용할때
+
+def onConnect(client, userdata, flag, rc):
+    print("Connect with result code:" + str(rc))
+    client.subscribe("command", qos=0)    # command라는 토픽 데이터 수신 요청
+    pass
+
+
+def onMessage(client, userdata, msg):
+    command = str(msg.payload.decode("utf-8"))
+    if command == 'temperature':
+        print("온도 정보 전달 완료")
+        print(getTemperatureAndHumidity())
+        client.publish(
+            "temperature", getTemperatureAndHumidity(), qos=0)
+    pass
+
+
+# import 되지않고, 별도의 라즈베리파이에서 작동하는 상황을 가정
+if __name__ == '__main__':
+    import paho.mqtt.client as mqtt
+    # mqtt 사용용
+    broker_address = "localhost"
+
+    client = mqtt.Client()
+    client.on_connect = onConnect
+    client.on_message = onMessage
+
+    client.connect(broker_address, 1883)
+    client.loop_start()
+
+    while (True):
+        time.sleep(0.1)  # 프로그램 종료 방지용
+
+
